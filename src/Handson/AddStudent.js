7f52a04655e6bf349f759e23ae1ff2e3de38fadb
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 11:10:17 -0700

    Update AddStudent.js

--- src/Handson/AddStudent.js
+++ src/Handson/AddStudent.js
@@ -1,10 +1,66 @@
-import React from "react";
- 
-const AddStudent=()=>{
-    return(
-        <>
-        <h1>Add Student</h1>
-        </>
-    )
-}
+import React, { useState, useContext } from "react";
+import Store from "../Store/Store";
+
+const AddStudent = () => {
+  const [newStudent, setNewStudent] = useState({
+    Name: "",
+    Age: "",
+    Course: "",
+    Batch: "",
+  });
+
+  const contextData = useContext(Store);
+
+  const handleChange = (e) => {
+    setNewStudent({
+      ...newStudent,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddStudent = () => {
+    contextData.dataFunc([...contextData.data, newStudent]);
+    setNewStudent({
+      Name: "",
+      Age: "",
+      Course: "",
+      Batch: "",
+    });
+  };
+
+  return (
+    <>
+      <label>Name</label>
+      <input
+        type="text"
+        name="Name"
+        value={newStudent.Name}
+        onChange={handleChange}
+      />
+      <label>Age</label>
+      <input
+        type="number"
+        name="Age"
+        value={newStudent.Age}
+        onChange={handleChange}
+      />
+      <label>Course</label>
+      <input
+        type="text"
+        name="Course"
+        value={newStudent.Course}
+        onChange={handleChange}
+      />
+      <label>Batch</label>
+      <input
+        type="text"
+        name="Batch"
+        value={newStudent.Batch}
+        onChange={handleChange}
+      />
+      <button onClick={handleAddStudent}>Add Student</button>
+    </>
+  );
+};
+
 export default AddStudent;
