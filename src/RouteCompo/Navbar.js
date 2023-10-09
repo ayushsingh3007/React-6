5abab60655c152ed827d5f8e48c6cdbabc51719a
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 12:27:20 -0700

    Update Navbar.js

--- src/RouteCompo/Navbar.js
+++ src/RouteCompo/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Home } from "../Handson/Home";
 import Student from "../Handson/Student";
 import { Contact } from "../Handson/Contact";
@@ -8,34 +8,22 @@ import AddStudent from "../Handson/AddStudent";
 import { StoreProvider } from "../Store/Store";
 
 function Navbar() {
-  const [stuData, setStuData] = useState([
-    { id: 1, Name: "shyam", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 2, Name: "Ram", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 3, Name: "ayush", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 4, Name: "piyush", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 5, Name: "gaurav", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 6, Name: "kishan", Age: 24, Course: "MERN", Batch: "oct" },
-    { id: 7, Name: "abhi", Age: 24, Course: "MERN", Batch: "oct" },
-  ]);
-
   return (
-    <StoreProvider value={{ data: stuData, dataFunc: setStuData }}>
-      <BrowserRouter>
-        <div id="navbar">
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/student">Student</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-        </div>
+    <BrowserRouter>
+      <div id="navbar">
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/student">Student</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </div>
 
-        <Routes>
-          <Route path="/student" element={<Student />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/edit/:index" element={<EditStudent />} />
-          <Route path="/add" element={<AddStudent />} /> {/* This line was missing */}
-        </Routes>
-      </BrowserRouter>
-    </StoreProvider>
+      <Routes>
+        <Route path="/student" element={<Student />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/edit/:index" element={<EditStudent />} />
+        <Route path="/add" element={<AddStudent />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
