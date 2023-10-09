8d26933eda3a2c42c2898c1a0ab02ab22f67ecf7
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 12:00:37 -0700

    Update Navbar.js

--- src/RouteCompo/Navbar.js
+++ src/RouteCompo/Navbar.js
@@ -1,55 +1,49 @@
 import React, { useState } from "react";
-import Store from "../Store/Store"; 
 import { Home } from "../Handson/Home";
 import Student from "../Handson/Student";
 import { Contact } from "../Handson/Contact";
 
-import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import EditStudent from "../Handson/EditStudent";
 import AddStudent from "../Handson/AddStudent";
+import { StoreProvider } from "../Store/Store";
 
-function Navbar(){
-    const [stuData,setStuData]=useState([
-        {id:1,Name:"shyam",Age:24,Course:"MERN", Batch:"oct"},
-        {id:2,Name:"Ram",Age:24,Course:"MERN", Batch:"oct"},
-        {id:3,Name:"ayush",Age:24,Course:"MERN", Batch:"oct"},
-        {id:4,Name:"piyush",Age:24,Course:"MERN", Batch:"oct"},
-        {id:5,Name:"gaurav",Age:24,Course:"MERN", Batch:"oct"},
-        {id:6,Name:"kishan",Age:24,Course:"MERN", Batch:"oct"},
-        {id:7,Name:"abhi",Age:24,Course:"MERN", Batch:"oct"},
-    ])
+function Navbar() {
+  const [stuData, setStuData] = useState([
+    { id: 1, Name: "shyam", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 2, Name: "Ram", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 3, Name: "ayush", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 4, Name: "piyush", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 5, Name: "gaurav", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 6, Name: "kishan", Age: 24, Course: "MERN", Batch: "oct" },
+    { id: 7, Name: "abhi", Age: 24, Course: "MERN", Batch: "oct" },
+  ]);
 
-    return (
-        <BrowserRouter >
-            <div id="navbar">
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/student">Student</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-            </div>
+  return (
+    <StoreProvider value={{ data: stuData, dataFunc: setStuData }}>
+      <BrowserRouter>
+        <div id="navbar">
+          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/student">Student</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </div>
 
-            <Routes>
-                <Route
-                    path='/student'
-                    element={
-                        <Store.Provider value={{ data: stuData, dataFunc: setStuData }}>
-                            <Student />
-                        </Store.Provider>
-                    }
-                />
-                <Route path='/home' element={<Home />} />
-                <Route path='/contact' element={<Contact />} />
-                <Route
-                    path='/edit/:index'
-                    element={
-                        <Store.Provider value={{ data: stuData, dataFunc: setStuData }}>
-                            <EditStudent />
-                        </Store.Provider>
-                    }
-                />
-                <Route path='/add' element={<AddStudent />} />
-            </Routes>
-        </BrowserRouter>
-    );
+        <Routes>
+          <Route
+            path="/student"
+            element={<Student />}
+          />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/edit/:index"
+            element={<EditStudent />}
+          />
+          <Route path="/add" element={<AddStudent />} />
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
+  );
 }
 
 export default Navbar;
