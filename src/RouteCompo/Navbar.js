ecc89c57f44f01175212f2b172a5908212150bb6
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 11:18:59 -0700

    Update Navbar.js

--- src/RouteCompo/Navbar.js
+++ src/RouteCompo/Navbar.js
@@ -7,51 +7,49 @@ import { Contact } from "../Handson/Contact";
 import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
 import EditStudent from "../Handson/EditStudent";
 import AddStudent from "../Handson/AddStudent";
+
 function Navbar(){
     const [stuData,setStuData]=useState([
         {id:1,Name:"shyam",Age:24,Course:"MERN", Batch:"oct"},
-        {id:2,Name:"Ram",Age:24,ourse:"MERN", Batch:"oct"},
+        {id:2,Name:"Ram",Age:24,Course:"MERN", Batch:"oct"},
         {id:3,Name:"ayush",Age:24,Course:"MERN", Batch:"oct"},
         {id:4,Name:"piyush",Age:24,Course:"MERN", Batch:"oct"},
         {id:5,Name:"gaurav",Age:24,Course:"MERN", Batch:"oct"},
         {id:6,Name:"kishan",Age:24,Course:"MERN", Batch:"oct"},
         {id:7,Name:"abhi",Age:24,Course:"MERN", Batch:"oct"},
-        
     ])
-    return(
-        <>
-        
+
+    return (
         <BrowserRouter >
-        <div id="navbar">
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/student">Student</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        
-        </div>
+            <div id="navbar">
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/student">Student</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
 
-        <Routes>
-        <Route path='/Student' element={<Store.Provider value={{data:stuData,dataFunc:setStuData}}>
-            <Student/>
-            </Store.Provider>
-            }/>
-           
-            <Route path='/home' element={<Home/>} />
-            <Route path='/contact' element={<Contact/>} />
-            <Route path='/edit' element={<Store.Provider value={{data:stuData,dataFunc:setStuData}}>
-            <EditStudent/>
-            </Store.Provider>
-            }
-               />
-            <Route path='/add' element={<AddStudent/>}/>
-            
-            
-
-
-        </Routes>
-        
+            <Routes>
+                <Route
+                    path='/student'
+                    element={
+                        <Store.Provider value={{ data: stuData, dataFunc: setStuData }}>
+                            <Student />
+                        </Store.Provider>
+                    }
+                />
+                <Route path='/home' element={<Home />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route
+                    path='/edit/:index'
+                    element={
+                        <Store.Provider value={{ data: stuData, dataFunc: setStuData }}>
+                            <EditStudent />
+                        </Store.Provider>
+                    }
+                />
+                <Route path='/add' element={<AddStudent />} />
+            </Routes>
         </BrowserRouter>
-        
-        </>
-    )
+    );
 }
+
 export default Navbar;
