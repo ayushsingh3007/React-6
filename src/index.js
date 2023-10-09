39ed5058e14924e9c39c9f2dd2aad915bdd93e4b
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 12:43:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom'; // Import createRoot from react-dom
-
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './Store/Store'; // Import StoreProvider
 
-const root = createRoot(document.getElementById('root')); // Use createRoot instead of render
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider> {/* Wrap App with StoreProvider */}
+      <App />
+    </StoreProvider>
   </React.StrictMode>
 );
 
