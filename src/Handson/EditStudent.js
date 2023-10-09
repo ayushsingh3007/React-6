656b3b2a404655da2a26ff88569e83a0790af5c5
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 11:03:41 -0700

    Update EditStudent.js

--- src/Handson/EditStudent.js
+++ src/Handson/EditStudent.js
@@ -1,56 +1,64 @@
-import React, { useContext } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Store from "../Store/Store";
 
-const EditStudent=()=>{
-    const Navigate=useNavigate();
-    const indexData=useLocation().state.index;
-    console.log(indexData)
+const EditStudent = () => {
+  const navigate = useNavigate();
+  const { index } = useParams();
+  const contextData = useContext(Store);
+  const [updateData, setUpdateData] = useState({
+    Name: contextData.data[index].Name,
+    Age: contextData.data[index].Age,
+    Course: contextData.data[index].Course,
+    Batch: contextData.data[index].Batch,
+  });
 
-    const contextData=useContext(Store);
-
-
-
-
-  const updateData={
-    Name:"contextData.data[indexData].Name",
-    Age:"contextData.data[indexData].Age",
-    Course:"contextData.data[indexData].Course",
-    Batch:"contextData.data[indexData].Batch"
+  function handleChange(e) {
+    setUpdateData({
+      ...updateData,
+      [e.target.name]: e.target.value,
+    });
   }
 
-    function handleChange(e){
-     updateData[e.target.name]=e.target.value;
-    }
+  const handleUpdate = () => {
+    contextData.data[index] = updateData;
+    navigate("/student");
+  };
 
+  return (
+    <>
+      <label>Name </label>
+      <input
+        type="text"
+        name="Name"
+        value={updateData.Name}
+        onChange={handleChange}
+      />
+      <label>Age </label>
+      <input
+        type="number"
+        name="Age"
+        value={updateData.Age}
+        onChange={handleChange}
+      />
+      <label>Course</label>
+      <input
+        type="text"
+        name="Course"
+        value={updateData.Course}
+        onChange={handleChange}
+      />
+      <label>Batch</label>
+      <input
+        type="text"
+        name="Batch"
+        value={updateData.Batch}
+        onChange={handleChange}
+      />
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={() => navigate("/student")}>Go Back</button>
+    </>
+  );
+};
 
-  
-
-
-
-
-    const handleUpdate=()=>{
-        console.log("handle click")
-        contextData.data[indexData]=updateData;
-    
-        Navigate('/student')
-    }
-
-
-    return(
-        <>
-        <label>Name </label>
-        <input type="text" name="Name" placeholder=" Enter Name" onChange={handleChange}/>
-        <label>Age </label>
-        <input type="number" name="Age" placeholder="Enter Age" onChange={handleChange}/>
-        <label>Course</label>
-        <input type="text" name="Course" placeholder="Enter Course" onChange={handleChange}/>
-        <label>Batch</label>
-        <input type="text" name="Batch" placeholder="Enter Batch" onChange={handleChange}/>  
-        <button onClick={handleUpdate}>Update</button>
-        <button onClick={()=>Navigate('/student')}>Go Back</button>
-        </>
-
-    )
-}
 export default EditStudent;
