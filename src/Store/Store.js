7e6461165ded7e013eb91750583beeecd64beb6e
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 11:45:37 -0700

    Update Store.js

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,4 +1,10 @@
-import  { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const Store=createContext();
-export default Store;
+const Store = createContext();
+
+const StoreProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+  return <Store.Provider value={{ data, dataFunc: setData }}>{children}</Store.Provider>;
+};
+
+export { Store, StoreProvider };
