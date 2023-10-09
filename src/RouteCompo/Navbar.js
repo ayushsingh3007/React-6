e0264143a94dc4333de7ffb94d8ae93645001edb
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 12:21:28 -0700

    Update Navbar.js

--- src/RouteCompo/Navbar.js
+++ src/RouteCompo/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Home } from "../Handson/Home";
 import Student from "../Handson/Student";
 import { Contact } from "../Handson/Contact";
-
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import EditStudent from "../Handson/EditStudent";
 import AddStudent from "../Handson/AddStudent";
@@ -29,17 +28,11 @@ function Navbar() {
         </div>
 
         <Routes>
-          <Route
-            path="/student"
-            element={<Student />}
-          />
+          <Route path="/student" element={<Student />} />
           <Route path="/home" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          <Route
-            path="/edit/:index"
-            element={<EditStudent />}
-          />
-          <Route path="/add" element={<AddStudent />} />
+          <Route path="/edit/:index" element={<EditStudent />} />
+          <Route path="/add" element={<AddStudent />} /> {/* This line was missing */}
         </Routes>
       </BrowserRouter>
     </StoreProvider>
