9242e09228347df8e4e64a31f5662bb228f60f20
ayushsingh3007 <131241505+ayushsingh3007@users.noreply.github.com>
2023-10-09 11:04:17 -0700

    Update Student.js

--- src/Handson/Student.js
+++ src/Handson/Student.js
@@ -1,56 +1,56 @@
-import React, { useContext } from "react"
-import { NavLink, useNavigate } from "react-router-dom"
+import React, { useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import Store from "../Store/Store";
 
+const Student = () => {
+  const displayData = useContext(Store);
+  const navigate = useNavigate();
 
-const Student=()=>{
-     const displayData=useContext(Store);
-     const Navi =useNavigate();
-     function handleChange(){
-        Navi('/add')
-     }
+  function handleEdit(index) {
+    navigate(`/edit/${index}`);
+  }
 
-    
-    return(
-        <>
+  function handleAddUser() {
+    navigate("/add");
+  }
 
-        <h1>Student Page</h1>
-        <button id="btn-1" onClick={handleChange}>add user</button>
+  return (
+    <>
+      <h1>Student Page</h1>
+      <button id="btn-1" onClick={handleAddUser}>
+        Add user
+      </button>
 
-        <table >
-            <thead>
-                <tr>
-                    <th>NAME </th>
-                    <th>BATCH</th>
-                    <th>COURSE</th>
-                    <th>CONTACT</th>
-                </tr>
-            </thead>
+      <table>
+        <thead>
+          <tr>
+            <th>NAME</th>
+            <th>BATCH</th>
+            <th>COURSE</th>
+            <th>CONTACT</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayData.data.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.Name}</td>
+                <td>{item.Batch}</td>
+                <td>{item.Course}</td>
+                <td>{item.Contact}</td>
+                <td>
+                  <NavLink to="#" onClick={() => handleEdit(index)}>
+                    Edit
+                  </NavLink>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
-        {displayData.data.map((item,index)=>{
-            return(
-            <tbody key={index}>
-                <tr >
-                    <td>{item.Name}</td>
-                    <td>{item.Batch}</td>
-                    <td>{item.Course}</td>
-                    <td><NavLink to="/edit" state={{index}}>Edit</NavLink></td>
-                    
-                    
-                </tr>
-            </tbody>)
-        }
-        )}
-
-</table>
-
-
-
-
-
-
-        
-        </>
-    )
-}
 export default Student;
